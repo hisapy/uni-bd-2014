296760eff3b09018827a0ee96b1d146bec2e8f19
hisa <hisa@iporaitech.com>
2014-04-21 23:54:30 -0400

    * Accepting edits

--- indexeddb-app/js/controllers/todo_controller.js
+++ indexeddb-app/js/controllers/todo_controller.js
@@ -2,6 +2,15 @@ Todos.TodoController = Ember.ObjectController.extend({
 	actions : {
 		editTodo : function() {
 			this.set('isEditing', true);
+		},
+		acceptChanges : function() {
+			this.set('isEditing', false);
+
+			if (Ember.isEmpty(this.get('model.title'))) {
+				this.send('removeTodo');
+			} else {
+				this.get('model').save();
+			}
 		}
 	},
 
