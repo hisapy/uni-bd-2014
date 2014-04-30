0405bb814495374a568c0c4a0a8b0dfdc895d6a6
hisa <hisa@iporaitech.com>
2014-04-30 13:49:58 -0400

    Adding child routes

--- indexeddb-app/js/router.js
+++ indexeddb-app/js/router.js
@@ -1,11 +1,19 @@
 Todos.Router.map(function() {
 	this.resource('todos', {
 		path : '/'
+	}, function() {
+		// additional child rutes will go there later
 	});
 });
 
 Todos.TodosRoute = Ember.Route.extend({
 	model : function() {
 		return this.store.find('todo');
+	}
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+	model : function() {
+		return this.modelFor('todos');
 	}
 });
