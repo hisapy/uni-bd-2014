da91b73dd6e756cd76a78c35ad72be68baa4419a
hisa <hisa@iporaitech.com>
2014-04-30 12:25:48 -0400

    Deleting a Model

--- indexeddb-app/js/controllers/todo_controller.js
+++ indexeddb-app/js/controllers/todo_controller.js
@@ -11,6 +11,11 @@ Todos.TodoController = Ember.ObjectController.extend({
 			} else {
 				this.get('model').save();
 			}
+		},
+		removeTodo : function() {
+			var todo = this.get('model');
+			todo.deleteRecord();
+			todo.save();
 		}
 	},
 
